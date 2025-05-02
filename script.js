feb383918d6b74a94ed95402262ad8ee15295b7c
Mohammad Reza Abbasi <mohammadrezaabbasi344@gmail.com>
2025-05-02 17:31:17 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// select element 
+const iconList = document.querySelector('.nema') // icon |||
+const iconX = document.querySelector('.hide')
+const menuHam = document.querySelector('.menu-ham')
+// select element 
+
+
+iconList.addEventListener('click', () => {
+    iconList.classList.add('hidden')
+    iconX.classList.remove('hidden')
+    menuHam.style.transform = 'translateX(-25px)'
+})
+
+iconX.addEventListener('click', () => {
+    iconX.classList.add('hidden')
+    iconList.classList.remove('hidden')
+    menuHam.style.transform = 'translateX(-1000px)'
+})
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
